111f4cb92be16b864956a375c1381289aa100d63
Ikhsandi15 <sandiikhsandic@gmail.com>
2024-05-05 18:56:47 +0700

    minor: set nullable column regId

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('regId');
+            $table->string('regId')->nullable();
             $table->string('name');
             $table->string('phone_number')->unique();
             $table->string('profile')->nullable();

033a017161826f0e6daad985b63bd9c45fbfe70e
Ikhsandi15 <sandiikhsandic@gmail.com>
2024-05-07 21:13:29 +0700

    minor: replace response & type onDelete()

--- database/migrations/2024_05_04_103414_add_relationship_schools_to_users.php
+++ database/migrations/2024_05_04_103414_add_relationship_schools_to_users.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreign('school_id')->references('id')->on('schools')->onDelete('cascade');
+            $table->foreign('school_id')->references('id')->on('schools')->onDelete('restrict');
         });
     }
 

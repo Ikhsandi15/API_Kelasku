6941fa811943b36ee81ddfe2baed084e5ae7d7c1
Ikhsandi15 <sandiikhsandic@gmail.com>
2024-06-27 01:28:03 +0700

    refactor:  Update sendMessageToAndroid function to improve FCM notification sending

--- app/Http/Controllers/FirebaseController.php
+++ app/Http/Controllers/FirebaseController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use GuzzleHttp\Client;
+use App\Helpers\Helper;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Auth;
 
 class FirebaseController extends Controller
@@ -14,20 +16,36 @@ class FirebaseController extends Controller
     {
         $client = new Client();
 
-        $response = $client->post('https://fcm.googleapis.com/fcm/send', [
-            'headers' => [
-                'Authorization' => 'key=' . env('API_FCM_KEY'),
-                'Content-Type' => 'application/json',
-            ],
-            'json' => [
-                'to' => $regId,
-                'notification' => [
-                    'title' => 'Hey kamu di colek',
-                    'body' => 'Kamu di colek sama ' . User::where('id', Auth::id())->pluck('name')->first(),
-                ],
-            ],
-        ]);
+        // Mendapatkan nama pengguna
+        $userName = User::where('id', Auth::id())->pluck('name')->first();
+        if (!$userName) {
+            return response()->json(['error' => 'User not found'], 404);
+        }
 
-        return $response->getBody();
+        // Mempersiapkan payload untuk FCM
+        $payload = [
+            'to' => $regId,
+            'notification' => [
+                'title' => 'Hey kamu di colek',
+                'body' => 'Kamu di colek sama ' . $userName,
+            ],
+        ];
+
+        try {
+            // Mengirim permintaan POST ke FCM
+            $response = $client->post('https://fcm.googleapis.com/fcm/send', [
+                'headers' => [
+                    'Authorization' => 'key=' . env('API_FCM_KEY'),
+                    'Content-Type' => 'application/json',
+                ],
+                'json' => $payload,
+            ]);
+
+            return Helper::APIResponse("Notification sent successfully", 200, null, json_decode($response->getbody(), true));
+        } catch (\Exception $e) {
+            Log::error('Error sending FCM notification: ' . $e->getMessage());
+
+            return Helper::APIResponse("Failed to send notification", 500, "Internal Server Error", null);
+        }
     }
 }
